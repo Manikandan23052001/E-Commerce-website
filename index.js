a0cb2d994e9be1f27d81d3d83aa83afa93d8684f
Mani <abc@gmail.com>
2024-12-12 23:58:46 +0530

    first update

---
+++ index.js
@@ -0,0 +1,11 @@
+ var sidenav =document.querySelector(".side-navbar")
+//  sidenav.style.display ="none"
+
+ function showNavebar(){
+    sidenav.style.left= 0
+ }
+
+ function closeNavbar(){
+     sidenav.style.left="-60%"
+  
+ }
